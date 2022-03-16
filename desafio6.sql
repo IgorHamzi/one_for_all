379cf2441973984c30499f954de53e9ff061e2cf
IgorHamzi <igorwhamzi@gmail.com>
2022-03-16 12:10:58 -0300

    adiciona requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT 
+	ROUND(MIN(plans.value), 2) AS faturamento_minimo,
+    ROUND(MAX(plans.value), 2) AS faturamento_maximo,
+    ROUND(AVG(plans.value), 2) AS faturamento_medio,
+    ROUND(SUM(plans.value), 2) AS faturamento_total
+FROM 
+	SpotifyClone.user AS user
+JOIN 
+	SpotifyClone.plans AS plans ON plans.plan_id = user.plan_id
