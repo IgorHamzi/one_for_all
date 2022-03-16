ffff97ce89f029ce1b0303d1de6f7172558ad919
IgorHamzi <igorwhamzi@gmail.com>
2022-03-16 12:17:35 -0300

    adiciona requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,14 @@
+SELECT 
+	artists.name AS artista,
+    albums.name AS albums,
+    COUNT(follow.follow_id) AS seguidores
+FROM 
+	SpotifyClone.albums AS albums
+JOIN 
+	SpotifyClone.artists AS artists ON artists.artist_id = albums.artist_id
+JOIN 
+	SpotifyClone.follow AS follow ON follow.artist_id = albums.artist_id
+GROUP BY 
+	artists.name, albums.name
+ORDER BY 
+	seguidores DESC, artists.name ASC, albums.name ASC
