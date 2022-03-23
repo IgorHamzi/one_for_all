8ac1403400daff8e7d0dd12de05ac3d73a689af7
IgorHamzi <igorwhamzi@gmail.com>
2022-03-22 21:44:20 -0300

    atualiza requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT 
-	ROUND(MIN(plans.value), 2) AS faturamento_minimo,
-    ROUND(MAX(plans.value), 2) AS faturamento_maximo,
-    ROUND(AVG(plans.value), 2) AS faturamento_medio,
-    ROUND(SUM(plans.value), 2) AS faturamento_total
+	CONCAT(CAST((ROUND(MIN(plans.value), 2)) AS CHAR(3)), '.00') AS faturamento_minimo,
+    CAST((ROUND(MAX(plans.value), 2)) AS DECIMAL(12,2)) AS faturamento_maximo,
+    CAST((ROUND(AVG(plans.value),2)) AS DECIMAL(12,2)) AS faturamento_medio,
+    CAST((ROUND(SUM(plans.value), 2)) AS DECIMAL(12,2)) AS faturamento_total
 FROM 
 	SpotifyClone.user AS user
 JOIN 
